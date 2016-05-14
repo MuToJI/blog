f24df5b2db53235d520862b066466b49e68944cd
Alex K <fabian91alex@mail.ru>
2016-05-14 14:54:31 +0300

    основа

--- blog.php
+++ blog.php
@@ -1,1 +1,13 @@
 <?php
+
+$files = glob (__DIR__ . '/../messages/*');
+
+?>
+
+<?php
+foreach ($files as $file): ?>
+<hl><?= basename($file) ?></hl>
+<div><?= nl2br(htmlentities(file_get_contents($file))) ?><div>
+        <?php endforeach; ?>
+
+
