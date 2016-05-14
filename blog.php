d3549c38e5884c07e6d5f7297cf8402639262143
Alex K <fabian91alex@mail.ru>
2016-05-14 15:17:27 +0300

    добавлениеоконввода

--- blog.php
+++ blog.php
@@ -1,13 +1,21 @@
 <?php
 
-$files = glob (__DIR__ . '/../messages/*');
+$files = glob(__DIR__ . '/../messages/*');
 
 ?>
-
+<html xmlns="http://www.w3.org/1999/html">
+<head></head>
+<body>
 <?php
 foreach ($files as $file): ?>
-<hl><?= basename($file) ?></hl>
-<div><?= nl2br(htmlentities(file_get_contents($file))) ?><div>
-        <?php endforeach; ?>
-
+    <hl><?= basename($file) ?></hl>
+    <div><?= nl2br(htmlentities(file_get_contents($file))) ?></div>
+<?php endforeach; ?>
+<form method="GET">
+    <input type="text" name="title"></br>
+    <textarea name="message" id="" cols="30" rows="10"></textarea>
+    <input type="submit"/>
+</form>
+</body>
+</html>
 
